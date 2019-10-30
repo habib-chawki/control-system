445fce5850c6b77ee4b2c7cb58a04593a7d54ffa
habib-chawki <touati.habib.chawki@gmail.com>
2019-10-30 14:40:45 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,85 @@
+const http = require("http");
+const fs = require("fs");
+const WebSocket = require("ws");
+const firmata = require("firmata");
+
+const wss = new WebSocket.Server({ port: 8888 });
+
+let board = new firmata.Board("/dev/ttyACM0", () => {
+  //Potentiometer pins
+  board.pinMode(0, board.MODES.ANALOG);
+  board.pinMode(1, board.MODES.ANALOG);
+
+  //DC motor pins
+  board.pinMode(2, board.MODES.OUTPUT);
+  board.pinMode(3, board.MODES.PWM);
+});
+
+fs.readFile("./index.html", (err, html) => {
+  if (!err) {
+    http
+      .createServer((req, res) => {
+        res.writeHeader(200, { "Content-Type": "text/html" });
+        res.write(html);
+        res.end();
+      })
+      .listen(8080, "192.168.1.223", () => {
+        console.log("Server running ...");
+      });
+  }
+});
+
+let sendValue = function() {},
+  values = {},
+  dcMotorSpeed,
+  error,
+  inputToHBridge;
+const maxRightTurn = 875,
+  maxLeftTrun = 135,
+  factor = 2.5;
+
+board.on("ready", () => {
+  board.analogRead(0, value => {
+    error = values.valuePot1 - values.valuePot2;
+
+    //control DC motor speed
+    inputToHBridge = Math.abs(error) * factor;
+    dcMotorSpeed = inputToHBridge > 255 ? 255 : inputToHBridge;
+
+    console.log("motor speed: " + dcMotorSpeed + " pot: " + values.valuePot2);
+
+    board.analogWrite(3, dcMotorSpeed);
+
+    //control DC motor direction .. HIGH for right .. LOW for left
+    error > 0
+      ? board.digitalWrite(2, board.HIGH)
+      : board.digitalWrite(2, board.LOW);
+
+    //set limits for maximum turns
+    if (value > maxRightTurn) value = maxRightTurn;
+    else if (value < maxLeftTrun) value = maxLeftTrun;
+
+    //set the new pivot
+    //pivot = value;
+
+    values.valuePot1 = value;
+    sendValue(JSON.stringify(values));
+  });
+
+  board.analogRead(1, value => {
+    values.valuePot2 = value;
+    sendValue(JSON.stringify(values));
+  });
+});
+
+wss.on("connection", ws => {
+  sendValue = values => {
+    if (ws.readyState === WebSocket.OPEN) {
+      try {
+        ws.send(values);
+      } catch (e) {
+        console.log("Something went wrong ... " + e);
+      }
+    }
+  };
+});
